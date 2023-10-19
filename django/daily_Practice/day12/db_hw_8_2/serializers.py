af9255639fc6aed97c4d9d660f83abe5d99eef25
Siabel <jwj970802@gmail.com>
2023-10-19 17:23:05 +0900

    DRF 2 (N:1)

---
+++ django/daily_Practice/day12/db_hw_8_2/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import (__(a)__)
+
+
+class ArticleSerializer(__(b)__):
+
+    class Meta:
+        model = Article
+        fields = (__(c)__) # 개별 필드를 나열하지 말 것
+
+
+
+# (a) : Artcle
+# (b) : serializers.ModelSerializer
+# (c) : '__all__'
